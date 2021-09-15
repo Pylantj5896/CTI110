eb540cde72d5ba68f88235ffe69ec0d628792c3f
Pylantj5896 <90783195+Pylantj5896@users.noreply.github.com>
2021-09-15 11:27:54 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#CTI 110
-#P1LAB1
-#Jacob Pylant
-#9/15/2021
-
-userNum = int(input())
-userNumSquared = userNum ** 2   # Bug here; fix it when instructed
-   
-print(userNumSquared)       # Output formatting issue here; fix it when instructed
