40d2bd8e6fb5ff641b864c11df1f7d6da6d1fb9b
Pylantj5896 <90783195+Pylantj5896@users.noreply.github.com>
2021-09-15 11:17:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#CTI 110
+#P1LAB1
+#Jacob Pylant
+#9/15/2021
+
+userNum = int(input())
+userNumSquared = userNum ** 2   # Bug here; fix it when instructed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
